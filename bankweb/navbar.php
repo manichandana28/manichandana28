cad3c2201d64cc3c16b32a08c700a6d151a16f82
manichandana28 <59506833+manichandana28@users.noreply.github.com>
2021-05-19 20:43:13 +0530

    Add files via upload

---
+++ bankweb/navbar.php
@@ -0,0 +1,27 @@
+<!-- navbar -->
+<nav class="navbar navbar-expand-md navbar-light bg-dark">
+    <a href="index.php" class="navbar-brand" style="color: orange;"><b>SPARK Bank</b></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navabar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a href="index.php" class="nav-link" style="color: #3DC33F ;"><b>Home</b></a>
+            </li>
+            <!-- <li class="nav-item">
+                <a href="createuser.php" class="nav-link" style="color: #3DC33F ;"><b>Create User</b></a>
+            </li> -->
+            <li class="nav-item">
+                <a href="transfermoney.php" class="nav-link" style="color: #3DC33F ;"><b>View all Customers</b></a>
+            </li>
+            <li class="nav-item">
+                <a href="transactionhistory.php" class="nav-link" style="color: #3DC33F ;"><b>Transaction History</b></a>
+            </li>
+
+        </ul>
+
+    </div>
+
+</nav>
